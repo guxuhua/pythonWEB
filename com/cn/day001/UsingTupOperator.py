66474ceaac5eda5c4a26ad631d1e132099a91dd2
guxuhua <946422564@qq.com>
2018-07-11 15:06:57 +0800

    调用模块：元祖操作

---
+++ com/cn/day001/UsingTupOperator.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# 调用模块：TupOperator
+t = ("apple","orange","banana","juice",1,2,3,4,"a","b")
+import TupOperator
+# 初始化
+tu = TupOperator.TupOperator(t)
+# 展示元祖中的元素
+tu.showListInfo()
